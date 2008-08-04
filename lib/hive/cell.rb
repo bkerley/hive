d98b9878a9d150af1955df09a9296a094f0a80a8
Bryce Kerley <bryce@worldmedia.net>
2008-08-04 09:04:16 -0400

    Fix a paradox problem

--- lib/hive/cell.rb
+++ lib/hive/cell.rb
@@ -22,21 +22,33 @@ module Hive
     attr_accessor :name
     attr_accessor :hive
     attr_accessor :filename
+    attr_accessor :parent
+    attr_accessor :tree
     
     def initialize(cell_name, hive)
       self.name = cell_name
       self.filename = Cell.file_for_cell name
       self.hive = hive
+      self.parent = hive.repo.commits.last.id
+      self.tree   = hive.repo.tree.id
       super()
+    end
+    
+    def reload
+      file = self.hive.repo.tree/self.filename
+      self.merge YAML::load(file.data)
     end
     
     def save
       serialized = YAML::dump(self)
       i = self.hive.repo.index
       i.add(self.filename, serialized)
-      parent = self.hive.repo.commits.last.id
-      prev_tree = self.hive.repo.commits.first.tree.id
+      parent = self.parent
+      prev_tree = self.tree
       i.commit("Saved #{self.name}", [parent], self.hive.actor, prev_tree)
+      self.parent = hive.repo.commits.last.id
+      self.tree   = hive.repo.tree.id
+      self.reload
     end
     
     def history
